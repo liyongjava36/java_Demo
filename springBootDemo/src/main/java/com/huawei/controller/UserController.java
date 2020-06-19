05dcd7edaf161d39d26ed4e79c6290bf7bc50e3c
liyong <921120793@qq.com>
2020-06-19 22:29:40 +0800

    springbootdemo

---
+++ springBootDemo/src/main/java/com/huawei/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.huawei.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String helloUser(){
+        return "hello user";
+    }
+}
